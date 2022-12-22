823b35acab5c21dae8e849a3847126fd7482c64b
Szymon Cogiel <szymi130801@gmail.com>
2022-12-22 09:19:17 +0100

    chnge_struct_files

--- main.py
+++ main.py
@@ -23,23 +23,23 @@ if __name__ == '__main__':
     start_time = time.time()
 
     gis = GIS(username=arcgis_user, password=arcgis_pass)
-    # print("---ARCGIS ONLINE CONECTED IN %s seconds ---" % (time.time() - start_time))
+    print("---ARCGIS ONLINE CONECTED IN %s seconds ---" % (time.time() - start_time))
     # find all stations
     r = requests.get('https://api.gios.gov.pl/pjp-api/rest/station/findAll')
     j = r.json()
     stacje = pd.json_normalize(j)
     stacje.to_csv('stacje.csv')
     now = datetime.datetime.now()
-
+    #
     stanowiska = pd.json_normalize({
         'id': 806, 'stationId': 142,
         'param': {'paramName': 'tlenek węgla', 'paramFormula': 'CO', 'paramCode': 'CO', 'idParam': 8}
     })
-
-    r = requests.get('https://api.gios.gov.pl/pjp-api/rest/data/getData/{0}'.format(92))
-    dic = r.json()
-    pomiarowe_PM10 = pd.json_normalize(dic['values'])
-    pomiarowe_CO = pomiarowe_PM10.copy()
+    #
+    # r = requests.get('https://api.gios.gov.pl/pjp-api/rest/data/getData/{0}'.format(92))
+    # dic = r.json()
+    pomiarowe_PM10 = pd.DataFrame({})
+    pomiarowe_CO = pd.DataFrame({})
     for sid in stacje['id']:
         # get Stanowiska pomiarowe
         r = requests.get('https://api.gios.gov.pl/pjp-api/rest/station/sensors/{0}'.format(sid))
@@ -52,9 +52,9 @@ if __name__ == '__main__':
         # indeks_jakosci_powietrza = r.json()
         #
         # print(indeks_jakosci_powietrza)
-    # print("---FIRST STAGE DOWNL IN %s seconds ---" % (time.time() - start_time))
-    # print(len(stanowiska['id'].unique()))
-    # i = 1
+    print("---FIRST STAGE DOWNL IN %s seconds ---" % (time.time() - start_time))
+    print(len(stanowiska['id'].unique()))
+    i = 1
     for sen_id in stanowiska['id'].unique()[:400]:
 
         r = requests.get('https://api.gios.gov.pl/pjp-api/rest/data/getData/{0}'.format(sen_id))
@@ -62,32 +62,31 @@ if __name__ == '__main__':
 
         if dane_pomiarowe['key'] == 'PM10':
             try:
-                val = pd.DataFrame.from_dict(pd.json_normalize(dane_pomiarowe['values'])['value'])
-                name = stacje[stacje['id'] == stanowiska[stanowiska['id'] == sen_id].iloc[0]['stationId']]\
+                val = pd.DataFrame.from_dict(pd.json_normalize(dane_pomiarowe['values']))
+                name = stacje[stacje['id'] == stanowiska[stanowiska['id'] == sen_id].iloc[0]['stationId']] \
                     .iloc[0]['stationName']
-                val.rename(columns={'value': str(name)}, inplace=True)
-                pomiarowe_PM10 = pomiarowe_PM10.merge(val, left_index=True, right_index=True)
+                val['stacja'] = name
+                pomiarowe_PM10 = pd.concat([pomiarowe_PM10, val], ignore_index=True)
             except Exception as e:
                 logger.error(e)
 
         if dane_pomiarowe['key'] == 'CO':
             try:
-                val = pd.DataFrame.from_dict(pd.json_normalize(dane_pomiarowe['values'])['value'])
-                name = stacje[stacje['id'] == stanowiska[stanowiska['id'] == sen_id].iloc[0]['stationId']]\
+                val = pd.DataFrame.from_dict(pd.json_normalize(dane_pomiarowe['values']))
+                name = stacje[stacje['id'] == stanowiska[stanowiska['id'] == sen_id].iloc[0]['stationId']] \
                     .iloc[0]['stationName']
-                val.rename(columns={'value': str(name)}, inplace=True)
-                pomiarowe_CO = pomiarowe_CO.merge(val, left_index=True, right_index=True)
+                val['stacja'] = name
+                pomiarowe_CO = pd.concat([pomiarowe_CO, val], ignore_index=True)
             except Exception as e:
                 logger.error(e)
-        # print(i)
-        # i += 1
 
-    # print("---LAST STAGE DOWNL IN %s seconds ---" % (time.time() - start_time))
+        print(i)
+        i += 1
+
+    print("---LAST STAGE DOWNL IN %s seconds ---" % (time.time() - start_time))
 
     stanowiska.to_csv('stanowsika.csv')
-    pomiarowe_PM10 = pomiarowe_PM10.drop(['value'], axis=1)
     pomiarowe_PM10.to_csv('pomiarowe_PM10.csv')
-    pomiarowe_CO = pomiarowe_CO.drop(['value'], axis=1)
     pomiarowe_CO.to_csv('pomiarowe_CO.csv')
 
     item_to_update = '1899af778abe4d809e870727ea602530'
@@ -104,14 +103,14 @@ if __name__ == '__main__':
     stanowiska_to_up.update(item_properties={
         "title": "stanowiska_aktualizowane", "description": "Data of update: " + str(now)})
 
-    item_to_update3 = 'bf82d5e07b9d4c97b0acb95ab79eee63'
+    item_to_update3 = 'bfa7b340680a4eefbb2b2bf0ade4e89f'
     pomiarowe_PM10_to_up = gis.content.get(item_to_update3)
     pomiarowe_PM10_to_up_flayer = FeatureLayerCollection.fromitem(pomiarowe_PM10_to_up)
     pomiarowe_PM10_to_up_flayer.manager.overwrite('pomiarowe_PM10.csv')
     pomiarowe_PM10_to_up.update(item_properties={
         "title": "pomiarowe_PM10_aktualizowane", "description": "Data of update: " + str(now)})
 
-    item_to_update4 = 'fae4d986da004b7b8747849edad75759'
+    item_to_update4 = '7d9c66e07263468e93ad266d931aa500'
     pomiarowe_CO_to_up = gis.content.get(item_to_update4)
     pomiarowe_CO_to_up_flayer = FeatureLayerCollection.fromitem(pomiarowe_CO_to_up)
     pomiarowe_CO_to_up_flayer.manager.overwrite('pomiarowe_CO.csv')
